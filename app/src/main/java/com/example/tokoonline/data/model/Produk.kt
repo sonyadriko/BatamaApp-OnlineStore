99a331368caa63c8f4d273473b00e72022d74586
Ainul Yaqin <muhammad.yaqin@amartha.com>
2023-09-11 01:22:21 +0700

    fix: conflict Produk.kt

--- app/src/main/java/com/example/tokoonline/data/model/Produk.kt
+++ app/src/main/java/com/example/tokoonline/data/model/Produk.kt
@@ -3,13 +3,11 @@ package com.example.tokoonline.data.model
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import com.google.gson.reflect.TypeToken
-import kotlinx.android.parcel.Parcelize
 import android.os.Parcelable
-import java.io.Serializable
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Produk(
-
     @SerializedName("image")
     val image: String = "",
     @SerializedName("nama")
@@ -20,17 +18,10 @@ data class Produk(
     val deskripsi: String = "",
     @SerializedName("id_users")
     val id_users: String? = "",
-
-//    lateinit var tag : String
-//    lateinit var nama : String
-//    lateinit var harga : String
-//    lateinit var deskripsi : String
-//    var gambar : Int = 0
-): Serializable, Parcelable {
+): Parcelable {
     fun toMap(): Map<String, Any?> {
         val gson = Gson()
         val json = gson.toJson(this)
         return gson.fromJson(json, object : TypeToken<Map<String, Any>>() {}.type)
     }
-
 }
