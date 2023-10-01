005127747c27e86609427c402890de6f7d3f508d
Ainul Yaqin <muhammad.yaqin@amartha.com>
2023-10-02 00:57:15 +0700

    update: add midtrans library, add product buildTypes fix: data not showing bcs of stupid notifyItemRangeChanged

---
+++ app/src/main/java/com/example/tokoonline/view/activity/CheckoutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tokoonline.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tokoonline.R
+
+class CheckoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
