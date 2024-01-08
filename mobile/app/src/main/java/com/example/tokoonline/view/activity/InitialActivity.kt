86349203e3a8c95614b46a923a5b411b54352f22
ai-null <ainul.y9b@gmail.com>
2024-01-09 02:55:02 +0700

    update initial screen shows statusbar - update input add custom filledbox input - update register and login

--- mobile/app/src/main/java/com/example/tokoonline/view/activity/InitialActivity.kt
+++ mobile/app/src/main/java/com/example/tokoonline/view/activity/InitialActivity.kt
@@ -1,9 +1,14 @@
 package com.example.tokoonline.view.activity
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import com.example.tokoonline.core.base.BaseAuthActivity
 import com.example.tokoonline.databinding.ActivityInitialBinding
 
+@Suppress("DEPRECATION", "ObsoleteSdkInt")
 class InitialActivity : BaseAuthActivity() {
 
     private lateinit var binding: ActivityInitialBinding
@@ -12,6 +17,16 @@ class InitialActivity : BaseAuthActivity() {
         binding = ActivityInitialBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (Build.VERSION.SDK_INT >= 19) {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
+
         binding.btnProsesLogin.setOnClickListener {
             goToLoginActivity()
         }
@@ -19,5 +34,16 @@ class InitialActivity : BaseAuthActivity() {
         binding.btnProsesDaftar.setOnClickListener {
             goToRegister()
         }
+    }
+
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
     }
 }
